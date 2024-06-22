f729d1081397a89a6d17bf64821bd1e03a14c491
davidsrj <davids.rj.2009@gmail.com>
2024-06-22 14:52:56 +0300

    added horizontal compare

--- Frontend/src/Compare.tsx
+++ Frontend/src/Compare.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import "./styles/Compare.css";
 
 interface ICompareProps {
@@ -7,22 +7,66 @@ interface ICompareProps {
 }
 export const Compare: React.FC<ICompareProps> = ({ element, overlay }) => {
   const [width, setWidth] = useState(400);
+  const [height, setHeight] = useState(300);
+  const [shiftPressed, setShiftPressed] = useState(false);
 
   const refCompare = useRef<HTMLDivElement>(null);
 
   const onSlide = (event: React.MouseEvent<HTMLDivElement>) => {
-    const cursorPosition =
-      event.pageX - (refCompare.current?.getBoundingClientRect()?.left ?? 0);
-    setWidth(cursorPosition);
+    if (shiftPressed) {
+      const cursorPosition =
+        event.pageY - (refCompare.current?.getBoundingClientRect()?.top ?? 0);
+      setHeight(cursorPosition);
+    } else {
+      const cursorPosition =
+        event.pageX - (refCompare.current?.getBoundingClientRect()?.left ?? 0);
+      setWidth(cursorPosition);
+    }
   };
 
   const onMouseLeave = () => {
-    setWidth(400);
+    if (shiftPressed) {
+      setHeight(300);
+    } else {
+      setWidth(400);
+    }
+  };
+
+  const handleKeyDown = (event: KeyboardEvent) => {
+    if (event.shiftKey && !shiftPressed) {
+      setShiftPressed(true);
+    }
+  };
+
+  const handleKeyUp = (event: KeyboardEvent) => {
+    if (!event.shiftKey && shiftPressed) {
+      setShiftPressed(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyDown);
+    window.addEventListener("keyup", handleKeyUp);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+      window.removeEventListener("keyup", handleKeyUp);
+    };
+  }, [shiftPressed]);
+
+  const drawRedLine = (size: number) => {
+    if (shiftPressed) {
+      return size === 300 ? "" : "red 0px 2px 0px";
+    } else {
+      return size === 400 ? "" : "red 2px 0px 0px";
+    }
   };
 
   return (
     <div
-      className="compare"
+      className={`compare ${
+        shiftPressed ? "cursor-row-resize" : "cursor-col-resize"
+      }`}
       onMouseMove={onSlide}
       onMouseLeave={onMouseLeave}
       onDragStart={(event) => event.preventDefault()}
@@ -30,8 +74,9 @@ export const Compare: React.FC<ICompareProps> = ({ element, overlay }) => {
       <div
         className="compare__visible"
         style={{
-          width: width,
-          boxShadow: width === 400 ? "" : "red 2px 0px 0px",
+          width: shiftPressed ? "100%" : width,
+          height: shiftPressed ? height : "100%",
+          boxShadow: drawRedLine(shiftPressed ? height : width),
         }}
         ref={refCompare}
       >
